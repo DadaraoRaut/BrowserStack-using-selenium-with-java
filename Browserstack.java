6740850b61a9f00873bd3110918bc30a2d9746ea
DadaraoRaut <115204873+DadaraoRaut@users.noreply.github.com>
2023-02-22 10:48:45 +0530

    Add files via upload

---
+++ Browserstack.java
@@ -0,0 +1,61 @@
+package test;
+
+import java.net.URL;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
+
+public class Browserstack 
+{
+	WebDriver driver;
+	@BeforeTest
+	public void setup()
+	{        
+	        String	username="BROWSERSTACK USERNAME";
+	        String  accessKey="BROWSERSTACK PASSWORD";
+		     
+            DesiredCapabilities cap = new DesiredCapabilities();
+            
+            cap.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
+            cap.setCapability("os_version", "10.0");
+            cap.setCapability("build","browserstack-build-1");
+            cap.setCapability("project","First TestNg Android Project");
+            cap.setCapability("name", "first_test");
+            cap.setCapability("device", "OnePlus OnePlus 7T");
+	
+		
+
+		try {
+			URL url=new URL("http://"+username+":"+accessKey+"@hub-cloud.browserstack/wd/hub");
+
+			driver=new RemoteWebDriver(url,cap);
+
+		}
+
+		catch(Exception e)
+		{
+			e.printStackTrace(); 
+		}
+	}
+	@Test
+	public void Firsttest() throws InterruptedException
+	{
+		
+		
+	}
+	@AfterTest
+	public void tearDown()
+	{
+      // driver.quit();
+	}
+
+
+
+}
